21e320583e5fdba7010dbfcc825da3ea993e8cc3
jcarbaugh <jcarbaugh@776a5302-3645-48a1-abd4-c79e1917cee9>
2008-10-28 18:31:05 +0000

    initial checkin
    git-svn-id: https://source.sunlightlabs.com/sunlightlabs/trunk@3 776a5302-3645-48a1-abd4-c79e1917cee9

---
+++ local_settings.py
@@ -0,0 +1,11 @@
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TEMPLATE_DIRS = (
+    '/Users/Jeremy/Projects/sunlightlabs/templates/',
+)
