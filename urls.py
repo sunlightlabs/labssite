567ee83f98f9ad75ac124f55859bc7788fc53121
jcarbaugh <jcarbaugh@776a5302-3645-48a1-abd4-c79e1917cee9>
2008-11-19 22:42:26 +0000

    template and blog updates
    git-svn-id: https://source.sunlightlabs.com/sunlightlabs/trunk@14 776a5302-3645-48a1-abd4-c79e1917cee9

--- urls.py
+++ urls.py
@@ -1,15 +1,25 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from blogdor.feeds import LatestPosts
 
 admin.autodiscover()
 
+class LabsLatestPosts(LatestPosts):
+    title = "Sunlight Labs Blog"
+    link = "/blog/feeds/latest/"
+    description = "Latest blog updates from the nerds at Sunlight Labs"
+
+blog_feeds = {
+    'latest': LabsLatestPosts,
+}
+
 urlpatterns = patterns('',
-    url(r'^admin/(.*)', admin.site.root, name='admin'),
-    #url(r'^blog/$', 'sunlightlabs.labs.views.blog', name="blog"),
-    url(r'^blog/comments/', include('django.contrib.comments.urls')),
-    url(r'^blog/', include('blogdor.urls')),
     url(r'^about/$', 'sunlightlabs.labs.views.about', name="about"),
+    url(r'^admin/(.*)', admin.site.root, name='admin'),
+    url(r'^blog/comments/', include('django.contrib.comments.urls')),
+    url(r'^blog/feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': blog_feeds}),
+    url(r'^blog/', include('blogdor.urls')),
     url(r'^projects/$', 'sunlightlabs.labs.views.projects', name="projects"),
     url(r'^$', 'sunlightlabs.labs.views.index', name='index'),
 )
