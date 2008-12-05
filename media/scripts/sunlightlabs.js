de2d38e4a5220e0abb99aa7b29e7ac458ca72579
jcarbaugh <jcarbaugh@776a5302-3645-48a1-abd4-c79e1917cee9>
2008-12-05 16:51:53 +0000

    add javascript
    git-svn-id: https://source.sunlightlabs.com/sunlightlabs/trunk@57 776a5302-3645-48a1-abd4-c79e1917cee9

---
+++ media/scripts/sunlightlabs.js
@@ -0,0 +1,11 @@
+var max_width = 400;
+$(function() {
+    $.getScript('http://services.sunlightlabs.com/brandingbar/hat_js/labs/');
+    $('.post_content img').load(function(i) {
+        var elem = $(this);
+        if (elem.width() > max_width) {
+            var r = elem.width() / elem.height();
+            elem.width(max_width).height(max_width / r);
+        }
+    });
+});
