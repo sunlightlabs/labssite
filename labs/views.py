1d6ef8734833f07397cd51e7cba6cf830a13a6aa
jcarbaugh <jcarbaugh@776a5302-3645-48a1-abd4-c79e1917cee9>
2009-04-06 19:05:06 +0000

    fix image wrapper
    git-svn-id: https://source.sunlightlabs.com/sunlightlabs/trunk@194 776a5302-3645-48a1-abd4-c79e1917cee9

--- labs/views.py
+++ labs/views.py
@@ -31,6 +31,6 @@ def contact_sent(request, form_class):
     return contact_form(request, form_class, template_name='contact_form/contact_form_sent.html')
     
 def image_wrapper(request, image_path):
-    image_path = "%simages/%s" % (settings.MEDIA_URL, image_path)
+    image_path = "images/%s" % image_path
     data = {"image_path": image_path}
     return render_to_response("labs/image_wrapper.html", data)
