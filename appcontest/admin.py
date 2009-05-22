f8610af4e7b8b850494981c6a72bfa73dbe285e8
jturk <jturk@776a5302-3645-48a1-abd4-c79e1917cee9>
2009-05-22 14:13:42 +0000

    added filtering to entries in admin
    git-svn-id: https://source.sunlightlabs.com/sunlightlabs/trunk@235 776a5302-3645-48a1-abd4-c79e1917cee9

--- appcontest/admin.py
+++ appcontest/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from sunlightlabs.appcontest.models import Contest,Entry
 
-class EntryAdmin(admin.ModelAdmin):
+class ContestAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Contest, EntryAdmin)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contest')
+    list_filter = ('contest',)
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Contest, ContestAdmin)
 admin.site.register(Entry, EntryAdmin)
