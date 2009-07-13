b686c238256e5650013202d2c62f444c6cca6ecf
James Turk <james.p.turk@gmail.com>
2009-07-13 16:54:24 -0400

    template changes for ideas

--- anthill/ideas/views.py
+++ anthill/ideas/views.py
@@ -1,11 +1,11 @@
 import datetime
 from django.template import RequestContext
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib.comments.models import Comment
 from django.contrib.contenttypes.models import ContentType
+from django.views.generic import list_detail
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
@@ -14,21 +14,10 @@ from models import Idea, Vote
 def idea_list(request, ordering='-total_upvotes'):
     ordering_db = {'most_popular': '-score',
                    'latest': '-submit_date'}[ordering]
-    paginator = Paginator(Idea.objects.order_by(ordering_db), 10)
-
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
-
-    try:
-        ideas = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        ideas = paginator.page(paginator.num_pages)
-
-    return render_to_response('ideas/index.html',
-                              {'ideas': ideas, 'ordering': ordering},
-                              context_instance=RequestContext(request))
+    return list_detail.object_list(request,
+        queryset=Idea.objects.select_related().order_by(ordering_db),
+        extra_context={'ordering': ordering}, paginate_by=10,
+        template_object_name='idea')
 
 def idea_detail(request, id):
     idea = get_object_or_404(Idea, pk=id)
