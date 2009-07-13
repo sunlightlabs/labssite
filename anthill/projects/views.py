a4b4ee3942dbbd204fc0e7f11fe4b62d506241f3
James Turk <james.p.turk@gmail.com>
2009-07-13 14:46:26 -0400

    select_related within projects

--- anthill/projects/views.py
+++ anthill/projects/views.py
@@ -5,16 +5,18 @@ from anthill.projects.models import Project
 from anthill.ideas.models import Idea
 
 def projects_and_ideas(request):
-    context = {'projects': Project.objects.all()[0:3],
-               'ideas': Idea.objects.all()[0:3]}
+    context = {'projects': Project.objects.select_related().all()[0:3],
+               'ideas': Idea.objects.select_related().all()[0:3]}
     return render_to_response('projects/projects_and_ideas.html', context,
                               context_instance=RequestContext(request))
 
 def archive(request):
-    return object_list(request, queryset=Project.objects.all(),
-                       template_object_name='project', allow_empty=True, 
+    return object_list(request,
+                       queryset=Project.objects.select_related().all(),
+                       template_object_name='project', allow_empty=True,
                        paginate_by=10)
 
 def project_detail(request, slug):
-    return object_detail(request, queryset=Project.objects.all(), slug=slug,
-                         template_object_name='project')
+    return object_detail(request, 
+                         queryset=Project.objects.select_related().all(),
+                         slug=slug, template_object_name='project')
