fc70e3544795e9b2ce57a22bf1ecc9447b6f8240
James Turk <james.p.turk@gmail.com>
2009-07-13 16:08:58 -0400

    minor changes to registration templates

--- media/scripts/labs.js
+++ media/scripts/labs.js
@@ -5,44 +5,42 @@ $(document).ready(function(){
     $("div#favoritePosts").hide();
     $("div#advancedSearch").hide();
 
-
-    $("form#standard_signupForm").hide();
-    $("form#standard_signinForm").hide();
+    //$("form#standard_signupForm").hide();
+    //$("form#standard_signinForm").hide();
 
     $("li#signup_standard a").click(function(){
         $("form#openid_signupForm").hide();
-    	$("form#standard_signupForm").show();
+        $("form#standard_signupForm").show();
     });
 
     $("li#signup_openid a").click(function(){
         $("form#standard_signupForm").hide();
-    	$("form#openid_signupForm").show();
+        $("form#openid_signupForm").show();
     });
 
     $("li#signin_standard a").click(function(){
         $("form#openid_signinForm").hide();
-    	$("form#standard_signinForm").show();
+        $("form#standard_signinForm").show();
+        return false;
     });
 
     $("li#signin_openid a").click(function(){
         $("form#standard_signinForm").hide();
-    	$("form#openid_signinForm").show();
+        $("form#openid_signinForm").show();
     });
 
     $("a#advancedBtn").click(function(){
-    	$("div#advancedSearch").toggle("slow");
+        $("div#advancedSearch").toggle("slow");
     });
-    
-    
+
     $("li#favorites a").click(function(){
-       	$("div#popularPosts").hide();
-       	$("div#favoritePosts").show();
+        $("div#popularPosts").hide();
+        $("div#favoritePosts").show();
     });
-    
+
     $("li#popular a").click(function(){
-       	$("div#favoritePosts").hide();
-       	$("div#popularPosts").show();
+        $("div#favoritePosts").hide();
+        $("div#popularPosts").show();
     });
-    
 
 });
