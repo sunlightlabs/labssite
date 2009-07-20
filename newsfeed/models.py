6fc1c5293895e449e43f659bd57c8feb527e1ee1
James Turk <james.p.turk@gmail.com>
2009-07-19 20:05:15 -0700

    newsfeed

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -5,16 +5,20 @@ class Feed(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
 
-class ItemType(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
-    icon_url = models.CharField(max_length=200, blank=True)
+    def __unicode__(self):
+        return self.title
+
 
 class FeedItem(models.Model):
     body = models.TextField(blank=True)
-    link = models.URLField(blank=True)
+    link = models.TextField(blank=True)
+    item_type = models.SlugField(max_length=20)
     user = models.ForeignKey(User)
-    item_type = models.ForeignKey(ItemType, related_name='items')
     feed = models.ForeignKey(Feed, related_name='items')
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-timestamp',)
+
+    def __unicode__(self):
+        return self.body
