19a8545c80d284a3f7c7d2cce915873ca9868324
James Turk <james.p.turk@gmail.com>
2009-07-24 15:40:28 -0700

    people s/TextField/MarkupField

--- anthill/people/models.py
+++ anthill/people/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from tagging.fields import TagField
+from markupfield.fields import MarkupField
 from anthill.models import LocationModel
 
 ROLES = (
@@ -15,7 +17,7 @@ class Profile(LocationModel):
     user = models.OneToOneField(User, related_name='profile')
     photo = models.ImageField(upload_to='profile_images', blank=True)
     url = models.URLField(blank=True)
-    about = models.TextField(blank=True)
+    about = MarkupField(blank=True, default_markup_type=settings.ANTHILL_DEFAULT_MARKUP)
     role = models.CharField(max_length=5, choices=ROLES, default='other')
     twitter_id = models.CharField(max_length=15, blank=True)
     skills = TagField('comma separated list of your skills (eg. python, django)')
