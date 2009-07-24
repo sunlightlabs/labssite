13b23b9368d3f53367e448457e50915c75772d69
James Turk <james.p.turk@gmail.com>
2009-07-23 20:51:53 -0700

    community profile charts

--- anthill/people/templatetags/people.py
+++ anthill/people/templatetags/people.py
@@ -1,6 +1,7 @@
 from django import template
 from django.conf import settings
-from anthill.utils import get_items_as_tag
+from django.db.models import Count
+from anthill.utils import get_items_as_tag, piechart_url, piechart_from_tags
 from anthill.people.models import Profile
 
 register = template.Library()
@@ -26,3 +27,18 @@ def possessive(user):
 @register.simple_tag
 def num_registered_users():
     return Profile.objects.all().count()
+
+@register.simple_tag
+def people_skills():
+    return piechart_from_tags(Profile, {'python':'00aa00', 'ruby':'aa0000'})
+
+@register.simple_tag
+def people_roles():
+    colors = {'dev': '003300', 'des': '009900', 
+              'both': '00cc00', 'other': '00ff00'}
+    items = Profile.objects.values('role').annotate(num=Count('id'))
+    for item in items:
+        item['name'] = name = item.pop('role')
+        item['color'] = colors[name]
+    return piechart_url(items)
+
--- anthill/utils.py
+++ anthill/utils.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from django.contrib.contenttypes.models import ContentType
+from tagging.models import Tag
 
 class SimpleItemsNode(template.Node):
     def __init__(self, queryset, count, offset, varname):
@@ -30,24 +33,26 @@ def get_items_as_tag(token, queryset, count=5):
 
 ## google charts ##
 
-def _piechart_url(items, width=200, height=200):
+def piechart_url(items, width=200, height=200):
     nums = []
     names = []
     colors = []
     for item in items:
-        nums.append(item['num'])
+        nums.append(str(item['num']))
         names.append(item['name'])
         colors.append(item['color'])
 
     chart_data = {'width': width, 'height': height, 'nums':','.join(nums),
                   'names':'|'.join(names), 'colors':'|'.join(colors)}
 
-    return 'http://chart.apis.google.com/chart?cht=p&chd=t:%(nums)s&chs=%(width)dx%(height)d&chl=%(names)s&chco=%(colors)s' % chart_data
+    img_url = 'http://chart.apis.google.com/chart?cht=p&chf=bg,s,00000000&chd=t:%(nums)s&chs=%(width)dx%(height)d&chdl=%(names)s&chco=%(colors)s' % chart_data
+    return '<img src="%s" />' % img_url
+piechart_url.mark_safe = True
 
-def _piechart_from_tags(model, tags, width=200, height=200):
+def piechart_from_tags(model, tags, width=200, height=200):
     ct = ContentType.objects.get_for_model(model).id
-    tag_names = tags.items()
+    tag_names = tags.keys()
     items = Tag.objects.filter(items__content_type=ct, name__in=tag_names).values('name').annotate(num=Count('id'))
     for item in items:
-        item['color'] = items[item['name']]
-    return _piechart_url(items, width, height)
+        item['color'] = tags[item['name']]
+    return piechart_url(items, width, height)
