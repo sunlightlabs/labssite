962a41abff8673cf88e60d2c8b76f55372d4302a
James Turk <james.p.turk@gmail.com>
2009-07-26 04:37:16 -0400

    events on bottom of page

--- anthill/events/models.py
+++ anthill/events/models.py
@@ -1,17 +1,31 @@
+import datetime
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
-from anthill.models import LocationModel
+from anthill.models import LocationModel, LocationModelManager
 from markupfield.fields import MarkupField
+
+class EventManager(LocationModelManager):
+    def future(self):
+        now = datetime.datetime.now()
+        return self.filter(Q(start_date__gt=now)|Q(end_date__gt=now))
+
 
 class Event(LocationModel):
     title = models.CharField(max_length=100)
     description = MarkupField(default_markup_type=settings.ANTHILL_DEFAULT_MARKUP)
+    official = models.BooleanField(default=False)
     url = models.URLField(blank=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(null=True, blank=True)
     creator = models.ForeignKey(User)
+
+    objects = EventManager()
+
+    class Meta:
+        ordering = ['-start_date']
 
     def __unicode__(self):
         return self.title
