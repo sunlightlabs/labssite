daaca088671317121d39b133d03d3523f3d82903
James Turk <james.p.turk@gmail.com>
2009-07-27 00:55:40 -0400

    active projects

---
+++ anthill/projects/templatetags/projects.py
@@ -0,0 +1,13 @@
+from django import template
+from django.db.models import Count
+from anthill.utils import get_items_as_tag, piechart_from_tags
+from anthill.projects.models import Project
+from newsfeed.models import FeedItem
+
+register = template.Library()
+
+@register.tag
+def get_active_projects(parser, token):
+    most_active = FeedItem.objects.values_list('feed__slug', flat=True).order_by('count').annotate(count=Count('id'))[:5]
+    projects = Project.objects.filter(slug__in=list(most_active))
+    return get_items_as_tag(token, projects)
