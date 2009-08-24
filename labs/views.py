89f81b592e0be7e77f23d9a78fcf25bdf4d78c9c
James Turk <james.p.turk@gmail.com>
2009-08-24 13:02:07 -0400

    fixed aa2judging

--- labs/views.py
+++ labs/views.py
@@ -31,14 +31,14 @@ def aa2_judging(request):
     qs = QuestionSet.objects.get(slug='aa2public')
     try:
         aset = AnswerSet.objects.get(question_set=qs, user=request.user)
-        message = 'Your vote has been recorded, you may change it if you wish.'
+        answer = aset.answers.all()[0].text
     except AnswerSet.DoesNotExist:
         aset = None
-        message = None
+        answer = None
 
     form = SurveyForm(qs, answer_set=aset)
 
     return render_to_response('labs/public_judging.html',
-                              {'form': form, 'message': message},
+                              {'form': form, 'answer': answer},
                              context_instance=RequestContext(request))
 
