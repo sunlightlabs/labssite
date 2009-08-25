9c2644f8cf93db9538e68d554c7d2e434833bfe2
James Turk <james.p.turk@gmail.com>
2009-08-25 13:00:54 -0400

    use last month for active projects

--- anthill/projects/templatetags/projects.py
+++ anthill/projects/templatetags/projects.py
@@ -1,3 +1,4 @@
+import datetime
 from django import template
 from django.db.models import Count
 from anthill.utils import get_items_as_tag, chart_from_tags, _extract_chart_params, ChartNode
@@ -8,7 +9,8 @@ register = template.Library()
 
 @register.tag
 def get_active_projects(parser, token):
-    most_active = FeedItem.objects.values_list('feed__slug', flat=True).order_by('count').annotate(count=Count('id'))[:5]
+    start_time = datetime.datetime.now() - datetime.timedelta(30)  # month ago
+    most_active = FeedItem.objects.filter(timestamp__gt=start_time).values_list('feed__slug', flat=True).order_by('count').annotate(count=Count('id'))[:5]
     projects = Project.objects.filter(slug__in=list(most_active))
     if hasattr(projects, '_gatekeeper'):
         projects = projects.approved()
