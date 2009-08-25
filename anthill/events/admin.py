0f3594ddb1577eb6cf22facc47c8fb663db142f7
James Turk <james.p.turk@gmail.com>
2009-08-25 16:11:29 -0400

    moved anthill to own project

--- anthill/events/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.gis import admin
-from anthill.events.models import Event
-
-admin.site.register(Event, admin.OSMGeoAdmin)
-
--- anthill/people/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.gis import admin
-from anthill.people.models import Profile
-
-admin.site.register(Profile, admin.OSMGeoAdmin)
