f9bb2831f2183772230a6eac783f3deeeb98d042
James Turk <james.p.turk@gmail.com>
2009-08-26 13:16:37 -0400

    appjudging goes away now

--- appjudging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^submit/$', 'simplesurvey.views.submit', name='appjudging_submit'),
-    url(r'^app/(?P<app_id>\d+)/$', 'labssite.appjudging.views.app', name='appjudging_app'),
-    url(r'^app/(?P<app_id>\d+)/scorecard/$', 'labssite.appjudging.views.app_scorecard', name='appjudging_appscorecard'),
-    url(r'^(?P<contest>.*)/scores/$', 'labssite.appjudging.views.scores', name='appjudging_scores'),
-    url(r'^(?P<contest>.*)/$', 'labssite.appjudging.views.index', name='appjudging_app_list'),
-)
