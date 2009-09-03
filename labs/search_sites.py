eb3de2e629b836c6fbfd8704b9ed8099f8b7b9bb
James Turk <james.p.turk@gmail.com>
2009-09-03 18:12:23 -0400

    search ideas/events/people

--- labs/search_sites.py
+++ labs/search_sites.py
@@ -2,6 +2,9 @@ from haystack import site
 from haystack import indexes
 from blogdor.models import Post
 from anthill.projects.models import Project
+from anthill.events.models import Event
+from anthill.people.models import Profile
+from brainstorm.models import Idea
 
 class PostIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
@@ -21,3 +24,24 @@ class ProjectIndex(indexes.SearchIndex):
         return Project.objects.all()
 site.register(Project, ProjectIndex)
 
+
+class IdeaIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='user')
+    pub_date = indexes.DateTimeField(model_attr='submit_date')
+site.register(Idea, IdeaIndex)
+
+
+class EventIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='creator')
+
+    def get_queryset(self):
+        return Event.objects.future()
+site.register(Event, EventIndex)
+
+
+class ProfileIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='user')
+site.register(Profile, ProfileIndex)
