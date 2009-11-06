a5189f6acbf47394881072d44c4f986fa94e9a3d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-11-06 17:01:36 -0500

    cleaned up display of events

--- labs/templatetags/hackathon_tags.py
+++ labs/templatetags/hackathon_tags.py
@@ -10,5 +10,5 @@ register = template.Library()
 def hackathon_events():
     events = Event.objects.filter(
                 start_date__gte=datetime.datetime(2009, 12, 12, 0, 0, 0),
-                start_date__lte=datetime.datetime(2009, 12, 13, 23, 59, 59))
+                start_date__lte=datetime.datetime(2009, 12, 13, 23, 59, 59)).order_by('-official', 'title')
     return render_to_string("labs/hackathon_events.html", {"event_list": events})
