a9c63771bb51725a9480e4b9b228e5506f9668a0
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-11-06 12:45:53 -0500

    add scheduled events to hackathon pages

---
+++ labs/templatetags/hackathon_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from django.db.models import Q
+from django.template.loader import render_to_string
+from anthill.events.models import Event
+import datetime
+
+register = template.Library()
+
+@register.simple_tag
+def hackathon_events():
+    events = Event.objects.filter(
+                start_date__gte=datetime.datetime(2009, 12, 12, 0, 0, 0),
+                start_date__lte=datetime.datetime(2009, 12, 13, 23, 59, 59))
+    return render_to_string("labs/hackathon_events.html", {"event_list": events})
