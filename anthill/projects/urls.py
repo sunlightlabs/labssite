a25c4677ac9f2ffb3e848f8804dd90cdc5ac702d
James Turk <james.p.turk@gmail.com>
2011-02-04 11:30:08 -0500

    drop old projects

--- anthill/projects/urls.py
+++ anthill/projects/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('anthill.projects.views',
-    url(r'^$', 'projects', name='projects'),
-    url(r'^asks/$', 'ask_list', name='all_project_asks'),
     url(r'^tag/(?P<tag>[^/]+)/$', 'tag_archive', name='projects_tagged'),
     url(r'^(?P<slug>[-\w]+)/$', 'project_detail', name='project_detail'),
 )
