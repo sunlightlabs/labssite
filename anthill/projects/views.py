6ee34fbdadddf317908ae76f96ec8a0cf5dc549a
James Turk <james.p.turk@gmail.com>
2011-06-24 15:56:27 -0400

    drop more anthill stuff

--- anthill/projects/views.py
+++ anthill/projects/views.py
@@ -9,45 +9,6 @@ from django.template.loader import render_to_string
 from tagging.views import tagged_object_list
 from anthill.projects.models import Project, Role, Ask
 
-def projects(request):
-    """
-        Combined view of latest projects
-
-        Template: projects/projects.html
-
-        Context:
-            projects - Latest projects
-    """
-    project_qs = Project.objects.select_related().order_by('-update_date').all()
-    if hasattr(project_qs, '_gatekeeper'):
-        project_qs = project_qs.approved()
-    context = {'projects': project_qs[:5],}
-    return render_to_response('projects/projects.html', context,
-                              context_instance=RequestContext(request))
-
-def archive(request, projects='all'):
-    """
-        Paginated listing of ``Project``s.
-
-        Template: projects/projects_list.html
-
-        Context:
-            projects     - 'all'/'official'/'community'
-            project_list - list of projects
-
-            See ``django.views.generic.list_detail.object_list`` for pagination variables.
-    """
-    qs = Project.objects.select_related().order_by('-update_date')
-    if projects == 'official':
-        qs = qs.filter(official=True)
-    elif projects == 'community':
-        qs = qs.filter(official=False)
-    if hasattr(qs, '_gatekeeper'):
-        qs = qs.approved()
-    return object_list(request, queryset=qs,
-                       template_object_name='project', allow_empty=True,
-                       extra_context={'projects':projects}, paginate_by=10)
-
 def tag_archive(request, tag):
     """
         Paginated listing of ``Project``s by tag.
@@ -81,19 +42,3 @@ def project_detail(request, slug):
     return object_detail(request,
                          queryset=Project.objects.select_related().all(),
                          slug=slug, template_object_name='project')
-
-def ask_list(request):
-    """
-        Paginated listing of ``Ask``s.
-
-        Template: projects/ask_list.html
-
-        Context:
-            ask_list - list of asks
-
-            See ``django.views.generic.list_detail.object_list`` for pagination variables.
-    """
-    return object_list(request,
-                       queryset=Ask.objects.select_related().all(),
-                       template_object_name='ask', allow_empty=True,
-                       paginate_by=20)
--- labs/templatetags/people.py
+++ labs/templatetags/people.py
@@ -1,10 +1,6 @@
 from django import template
-from django.conf import settings
-from django.db.models import Count
-from anthill.utils import get_items_as_tag, chart_from_tags, _extract_chart_params, ChartNode
 
 register = template.Library()
-
 
 @register.simple_tag
 def display_name(user):
