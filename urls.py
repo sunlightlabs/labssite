7da24c8288927cdbef0a0822a8c7f4f5d5fa9d73
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-02-04 10:46:50 -0500

    add more project redirects

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
+from django.views.generic.simple import redirect_to
 from blogdor.models import Post
 from labs.feeds import LabsLatestPosts, LabsLatestForTag, LabsLatestForAuthor
 from labs.forms import LabsContactForm
@@ -27,41 +28,45 @@ urlpatterns = patterns('',
     # apps
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include('haystack.urls')),
-    url(r'^projects/', include('anthill.projects.urls')),
+    # url(r'^projects/', include('anthill.projects.urls')),
     url(r'^wiki/', include('markupwiki.urls')),
 
     # blog/blogdor
     url(r'^blog/feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
         {'feed_dict': blog_feeds}, name="blogdor_feeds"),
-    url(r'^blog/(?P<year>\d{4})/(?P<slug>[\w-]+)/$', thegreatredirector),
-    url(r'^blog/', include('blogdor.urls')),
+    # url(r'^blog/', include('blogdor.urls')),
 
     # contact form
-    url(r'^contact/$', 'labs.views.contact_form', name='contact_form'),
+    # url(r'^contact/$', 'labs.views.contact_form', name='contact_form'),
 
     # redirects
-    url(r'^events/', 'django.views.generic.simple.redirect_to',
-        {'url': 'http://meetup.com/sunlightfoundation/'}),
-    url(r'^api/', 'django.views.generic.simple.redirect_to',
-        {'url': 'http://services.sunlightlabs.com/api/'}),
-    url(r'^people/', 'django.views.generic.simple.redirect_to',
-        {'url': '/about/'}),
-    url(r'^projects/all/', 'django.views.generic.simple.redirect_to',
-        {'url': '/projects/'}),
-    url(r'^projects/official/', 'django.views.generic.simple.redirect_to',
-        {'url': '/projects/'}),
-    url(r'^projects/community/', 'django.views.generic.simple.redirect_to',
-        {'url': '/projects/'}),
+    url(r'^events/', redirect_to, {'url': 'http://meetup.com/sunlightfoundation/'}),
+    url(r'^api/', redirect_to, {'url': 'http://services.sunlightlabs.com/api/'}),
+    # url(r'^people/', redirect_to, {'url': '/about/'}),
+    # url(r'^projects/all/', redirect_to, {'url': '/projects/'}),
+    # url(r'^projects/official/', redirect_to, {'url': '/projects/'}),
+    # url(r'^projects/community/', redirect_to, {'url': '/projects/'}),
 
     # labs specific
     url(r'^images/(?P<image_path>.*)$', 'labs.views.image_wrapper',
         name="image_wrapper"),
-    url(r'^photobooth/$', 'django.views.generic.simple.redirect_to',
+    url(r'^photobooth/$', redirect_to,
         {'url': 'http://www.flickr.com/photos/sunlightfoundation/sets/72157632180391380/'}),
-    url(r'^people/(?P<username>\w+)/$', 'labs.views.profile_redirect',
-        name='user_profile'),
+    # url(r'^people/(?P<username>\w+)/$', 'labs.views.profile_redirect',
+    #     name='user_profile'),
     url(r'^$', 'labs.views.index', name='index'),
 )
+
+# shutdown redirects
+
+urlpatterns += patterns('',
+    url(r'^blog/(?P<year>\d{4})/(?P<slug>[\w-]+)/$', thegreatredirector),
+    url(r'^blog/', redirect_to, {'url': 'http://sunlightfoundation.com/blog/'}),
+    url(r'^contact/', redirect_to, {'url': 'http://sunlightfoundation.com/contact/'}),
+    url(r'^people/', redirect_to, {'url': 'http://sunlightfoundation.com/people/'}),
+    url(r'^projects/', redirect_to, {'url': 'http://sunlightfoundation.com/projects/'}),
+)
+
 
 if settings.DEBUG:
     urlpatterns += patterns('',
